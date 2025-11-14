445cf1fd7c7f41cd65eaf913bb61887057d81640
Yelyzaveta Osinna <yelyzaveta.osinna.shi.2024@lpnu.ua>
2025-11-14 06:09:27 +0200

    add features for lab 3.2

--- railway/railway/settings.py
+++ railway/railway/settings.py
@@ -38,7 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tickets',
+    'rest_framework',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.BasicAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ]
+}
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- railway/railway/urls.py
+++ railway/railway/urls.py
@@ -19,5 +19,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tickets.urls')),  # Підключили додаток tickets
+    path('api/', include('tickets.api_urls')),  # REST API
+    path('', include('tickets.urls')),  # веб-сайт
 ]
